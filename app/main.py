308e49599d81b68b90da7b31d68afec5cfe1ec29
Gagandeep Singh <gagandeepsingh2998@gmail.com>
2023-05-17 15:54:46 +0530

    add auth and user router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from .routers import cafe
+from .routers import cafe, auth, user
 from . import models, schemas
 from .database import engine, SessionLocal, get_db
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,5 @@ app = FastAPI()
 
 
 app.include_router(cafe.router)
+app.include_router(auth.router)
+app.include_router(user.router)
