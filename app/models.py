f1babcbe66549a768ce6eeb32c0fcbfad62db7b6
Gagandeep Singh <gagandeepsingh2998@gmail.com>
2023-05-17 15:55:20 +0530

    add user model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float
-from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 
 class Cafe(Base):
@@ -16,3 +16,15 @@ class Cafe(Base):
     has_wifi = Column(Boolean, nullable=False)
     map_url = Column(String, nullable=False)
     seats = Column(Integer, nullable=False)
+
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"
+    ), nullable=False)
+    owner = relationship("User")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
